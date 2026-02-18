546456016891907bb89161c3c0da0be85e3b2e58
Chaffhickoryvelila <chaffvelila@gmail.com>
2026-02-18 09:32:21 +0800

    new changes

---
+++ JS/Facebooklogin.js
@@ -0,0 +1,21 @@
+var email_inp = document.getElementById("email");
+var password_inp = document.getElementById("password");
+
+var mymail = "chaffvelila@gmail.com";
+var password = "killbot";
+
+function loginfunction() {
+    var getemail = email_inp.value;
+    var getpassword = password_inp.value;
+
+    if (getemail == mymail && getpassword == password) {
+        window.location.href = "card.html";
+    } else {
+        alert("password and email incorrect");
+        email_inp.style = "border-color:red;color: red"; {
+            password_inp.style = "border-color:red;color: red"; {
+
+            }
+        }
+    }
+}
---
+++ PAGES/Facebooklogin.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Facebook</title>
+    <link rel="stylesheet" href="../CSS/facebooklogin.css">
+    <link rel="icon"
+        href="https://thf.bing.com/th/id/OIP.QqTYsxjnv4_257JFzrcwlAHaHa?w=187&h=187&c=7&r=0&o=7&cb=thfc1&pid=1.7&rm=3">
+
+
+<body class="fb">
+    <div class="facebookfont">
+        <h2 class="h2">facebook</h2>
+        <h2 class="p1"> Connect with friends and the<br> world
+            around you on facebook</h2>
+    </div>
+
+    <div class="div1">
+        <input id="email" type="text" class="text" placeholder="Email or phone Number"> <br> <br>
+
+        <input id="password" type="password" class="password" placeholder="Password"> <br>
+
+        <button class="loginbutton" onclick="loginfunction()">Log In</button> <br>
+        <p class="forgot">Forgot password?</p>
+        <div class="IP"></div>
+
+        <button class="createnewaccount"> <a class="createhref" href="Createaccount.php">Create new Account</a> </button>
+    </div>
+    <p class="underpar"><span class="TYPE" style="font-weight:bold;">Create a Page</span> for a celebrity, brand or business.</p>
+
+</body>
+<script src="../JS/Facebooklogin.js"></script>
+
+</html>
